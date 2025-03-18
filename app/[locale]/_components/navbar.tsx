a5c9173d0fd32c83f8f559de33af1b5330e90f91
tornike2002 <toko.toka2000@gmail.com>
2025-03-18 16:36:17 +0400

    add navbar links and logo

--- app/[locale]/_components/navbar.tsx
+++ app/[locale]/_components/navbar.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
+import { NavLinks } from "../../../lib/links";
+import { Link } from "@/navigation";
+import { useTranslations } from "next-intl";
+
 function Navbar() {
-  return <div>navbar</div>;
+  const t = useTranslations("NavLinks");
+  return (
+    <div className="bg-black text-white flex justify-between items-center px-5">
+      <div className="flex items-center ">
+        <Image
+          src="/assets/JG-FREE.png"
+          alt="Logo"
+          width={80}
+          height={80}
+          className="w-auto h-auto"
+          priority
+        />
+        <h2>JG BOXING</h2>
+      </div>
+      <div className="flex items-center gap-4 pl-10">
+        {NavLinks.map((link) => (
+          <Link key={link.id} href={link.path}>
+            {t(link.name)}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
---
+++ lib/links.ts
@@ -0,0 +1,5 @@
+export const NavLinks = [
+  { id: 1, path: "/", name: "home" }, 
+  { id: 2, path: "/trainer", name: "trainer" },
+  { id: 4, path: "/contact", name: "contact" },
+];
