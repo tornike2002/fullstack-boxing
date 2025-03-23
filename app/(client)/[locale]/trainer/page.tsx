dfde55e4d7fb027c686c1cd007865f22bec69ca1
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-03-24 00:57:18 +0400

    implement trainer header UI

--- app/(client)/[locale]/trainer/page.tsx
+++ app/(client)/[locale]/trainer/page.tsx
@@ -1,5 +1,11 @@
+import InstructorDesc from "./_components/instructorDesc";
+
 function TrainerPage() {
-  return <div>TrainerPage</div>;
+  return (
+    <div className="min-h-screen bg-black">
+      <InstructorDesc />
+    </div>
+  );
 }
 
 export default TrainerPage;
