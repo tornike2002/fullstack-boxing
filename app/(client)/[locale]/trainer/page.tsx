def976091bc01795de87e95b87a0b87b0a932cd8
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-03-25 03:43:20 +0400

    implement macbook animation in instructor page

--- app/(client)/[locale]/trainer/page.tsx
+++ app/(client)/[locale]/trainer/page.tsx
@@ -1,9 +1,11 @@
 import InstructorDesc from "./_components/instructorDesc";
+import { InstructorCards } from "./_components/instructorCards";
 
 function TrainerPage() {
   return (
     <div className="min-h-screen bg-black">
       <InstructorDesc />
+      <InstructorCards />
     </div>
   );
 }
