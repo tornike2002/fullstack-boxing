93641b60cda43835c6fc16fc90cddf41e5c96559
tornike2002 <toko.toka2000@gmail.com>
2025-03-26 12:56:28 +0400

    restore carousel

--- app/(client)/[locale]/trainer/page.tsx
+++ app/(client)/[locale]/trainer/page.tsx
@@ -1,11 +1,12 @@
 import InstructorDesc from "./_components/instructorDesc";
 import { HeroScrollDemo } from "./_components/heroScrollDemo";
+import { Carousel } from "./_components/carousel";
 
 function TrainerPage() {
   return (
     <div className="min-h-screen bg-black">
       <InstructorDesc />
-   
+      <Carousel />
       <HeroScrollDemo />
     </div>
   );
