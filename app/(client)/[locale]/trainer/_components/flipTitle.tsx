d3bd52f97cd45b3251ada10065d79cdedb1f26dc
tornike2002 <toko.toka2000@gmail.com>
2025-03-27 19:46:08 +0400

    trainslated half project

--- app/(client)/[locale]/trainer/_components/flipTitle.tsx
+++ app/(client)/[locale]/trainer/_components/flipTitle.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { FlipWords } from "@/components/ui/flip-words";
+import { useLocale } from "next-intl";
+import { useTranslations } from "use-intl";
 
 export function FlipTitle() {
-  const words = ["Body", "Mind", "Life"];
+  const locale = useLocale() as "en" | "ka" | "ru";
+  const t = useTranslations("trainer");
+  const translations = {
+    en: ["Body", "Mind", "Life"],
+    ka: ["სხეულისთვის", "გონებისთვის", "ცხოვრებისთვის"],
+    ru: ["Тело", "Разум", "Жизнь"],
+  };
+
+  const words = translations[locale];
 
   return (
     <div className="flex justify-center items-center px-4 font-mono tracking-tight mb-10 bg-black text-white py-6 w-full">
       <div className="text-4xl mx-auto font-normal text-neutral-300 dark:text-neutral-200">
-        Boxing: Power for <FlipWords words={words} />
+        {t("animatedTitle")} <FlipWords words={words} />
       </div>
     </div>
   );
